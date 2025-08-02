568ebb07bc34c15fb8e6da59f2362b06291d50f3
jsonBackup <jb-2000@gmx.de>
2025-08-03 01:02:19 +0200

    Implement SayHelloAgain method in Greeter service and update client to use it

--- 00_hello_world/greeter_server.cc
+++ 00_hello_world/greeter_server.cc
@@ -53,6 +53,13 @@ class GreeterServiceImpl final : public Greeter::Service {
     reply->set_message(prefix + request->name());
     return Status::OK;
   }
+
+  Status SayHelloAgain(ServerContext* context, const HelloRequest* request,
+                       HelloReply* reply) override {
+    std::string prefix("Hello again ");
+    reply->set_message(prefix + request->name());
+    return Status::OK;
+  }
 };
 
 void RunServer(uint16_t port) {
