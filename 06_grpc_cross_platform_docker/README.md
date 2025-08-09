49da81467918b1e5abed206bb6d53c9f9a69f931
jsonBackup <jb-2000@gmx.de>
2025-08-09 19:23:21 +0200

    Implement computeSum of gRPC server

--- 06_grpc_cross_platform_docker/README.md
+++ 06_grpc_cross_platform_docker/README.md
@@ -1,5 +1,6 @@
 ```sh
 docker build -t mygrpc .
 docker images
-docker run -it mygrpc
+docker run -d -p 50051:50051 --name mygrpc -v .:/root mygrpc sh -c "/root/server/build/server"
+docker logs -f mygrpc
 ```
--- 06_grpc_cross_platform_docker/server/src/server.cc
+++ 06_grpc_cross_platform_docker/server/src/server.cc
@@ -1,1 +1,26 @@
-int main() { return 0; }
+#include <grpcpp/grpcpp.h>
+
+#include "protos/hello.grpc.pb.h"
+#include "protos/hello.pb.h"
+
+class ProcessingImpl : public ProcessingServices::Service {
+  ::grpc::Status computeSum(::grpc::ServerContext* context,
+                            const ::Point3* request, ::Numeric* response) {
+    std::cout << "Called" << std::endl;
+    response->set_value(request->x() + request->y() + request->z());
+    return grpc::Status::OK;
+  }
+};
+
+int main() {
+  ProcessingImpl service;
+  grpc::ServerBuilder builder;
+  builder.AddListeningPort("0.0.0.0:50051", grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
+
+  std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+
+  server->Wait();
+
+  return 0;
+}
