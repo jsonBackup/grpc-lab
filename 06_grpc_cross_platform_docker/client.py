e25ca33f916978c43d64cb484c1a75b6eb40c0f7
jsonBackup <jb-2000@gmx.de>
2025-08-11 21:52:30 +0200

    Update README, client.py and add API client example image

--- 06_grpc_cross_platform_docker/client.py
+++ 06_grpc_cross_platform_docker/client.py
@@ -3,8 +3,8 @@ import hello_pb2
 import hello_pb2_grpc
 
 def run():
-    # Connect to the server at localhost:50051
-    channel = grpc.insecure_channel('localhost:50051')
+    # Change this to the server's IP address if running remotely
+    channel = grpc.insecure_channel('grpc-server:50051')
     stub = hello_pb2_grpc.ProcessingServicesStub(channel)
 
     point = hello_pb2.Point3(x=1.0, y=2.0, z=3.0)
